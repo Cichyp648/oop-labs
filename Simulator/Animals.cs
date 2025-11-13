ce64110d69539c8cc632bb542c185197d7d6d063
cichyp648 <cichyp648@gmail.com>
2025-11-13 23:28:48 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,9 +1,34 @@
-﻿namespace Simulator;
+﻿using System.Xml.Linq;
+
+namespace Simulator;
 
 public class Animals
 {
+    private string description = "Unknown";
+
     // kod z zadania
-    public required string Description { get; init; }
+    // dodana walidacja do init
+    public required string Description {
+        get { return description; }
+        init
+        {
+            string val = value.Trim();
+
+            if (val.Length < 3)
+                val = val.PadRight(3, '#');
+
+            if (val.Length > 25)
+                val = val.Substring(0, 15).TrimEnd();
+
+            if (val.Length < 3)
+                val = val.PadRight(3, '#');
+
+            if (char.IsLower(val[0]))
+                val = char.ToUpper(val[0]) + val.Substring(1);
+
+            description = val;
+        }
+    }
     public uint Size { get; set; } = 3;
 
     // właściwość odczytu Info
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,26 +2,54 @@
 
 public class Creature
 {
-    private string name;
-    private int level;
+    private string name = "Unknown"; // domyślne imie
+    private int level = 1; // domyślny poziom 1
 
     // odczyt imienia
     public string Name
     {
         get { return name; }
+        // walidacja imienia, init zamiast set - tylko jednorazowe ustalenie imienia
+        init
+        {
+            string val = value.Trim(' ');
+
+            if (val.Length < 3)
+                val = val.PadRight(3, '#');
+
+            if (val.Length > 25)
+                val = val.Substring(0, 25).TrimEnd();
+
+            if (val.Length < 3)
+                val = val.PadRight(3, '#');
+
+            if (char.IsLower(val[0]))
+                val = char.ToUpper(val[0]) + val.Substring(1);
+
+            name = val;
+        }
     }
 
-    // odczyt i walidacja przez set, wartości większe od zera, w przeciwnym razie default na 1
+    // odczyt imienia
     public int Level
     {
         get { return level; }
-        set { level = value > 0 ? value : 1; }
+        // walidacja przez init, ograniczenie do przedziału 1-10
+        init
+        {
+            if (value < 1)
+                level = 1;
+            else if (value > 10)
+                level = 10;
+            else
+                level = value;
+        }
     }
 
     // konstruktor z wartościami automatycznymi
     public Creature(string name = "Unnamed Monstrosity", int level = 1)
     {
-        this.name = name;
+        Name = name;
         Level = level;
     }
 
@@ -34,5 +62,16 @@ public class Creature
     public string Info
     {
         get { return $"{Name} [{Level}]"; }
+    }
+
+    public void Upgrade()
+    {
+        if (level < 10)
+        {
+            level++;
+            Console.WriteLine($"Creature {Name} has been upgraded to level {Level}!");
+        }
+        else
+            Console.WriteLine($"Creature {Name} cannot upgrade, level is already at the maximum!");
     }
 }
