f4a87466b57f538cd3ea12e93acd35fe2baf267c
cichyp648 <cichyp648@gmail.com>
2025-12-10 19:49:56 +0100

    TDD

--- Simulator/Maps/SmallSquareMap.cs
+++ Simulator/Maps/SmallSquareMap.cs
@@ -1,40 +1,37 @@
-﻿using System;
+﻿namespace Simulator.Maps;
 
-namespace Simulator.Maps
+public class SmallSquareMap : Map
 {
-    public class SmallSquareMap : Map
+    public int Size { get; }
+
+    private readonly Rectangle bounds;
+
+    public SmallSquareMap(int size)
     {
-        public int Size { get; }
+        if (size < 5 || size > 20)
+            throw new ArgumentOutOfRangeException(nameof(size),
+                "Map size must be between 5 and 20");
 
-        private readonly Rectangle bounds;
+        Size = size;
 
-        public SmallSquareMap(int size)
-        {
-            if (size < 5 || size > 20)
-                throw new ArgumentOutOfRangeException(nameof(size),
-                    "Map size must be between 5 and 20");
+        // obszar mapy (0,0)-(Size-1,Size-1)
+        bounds = new Rectangle(0, 0, Size - 1, Size - 1);
+    }
 
-            Size = size;
+    public override bool Exist(Point p)
+    {
+        return bounds.Contains(p);
+    }
 
-            // obszar mapy (0,0)-(Size-1,Size-1)
-            bounds = new Rectangle(0, 0, Size - 1, Size - 1);
-        }
+    public override Point Next(Point p, Direction d)
+    {
+        var np = p.Next(d);
+        return Exist(np) ? np : p;
+    }
 
-        public override bool Exist(Point p)
-        {
-            return bounds.Contains(p);
-        }
-
-        public override Point Next(Point p, Direction d)
-        {
-            var np = p.Next(d);
-            return Exist(np) ? np : p;
-        }
-
-        public override Point NextDiagonal(Point p, Direction d)
-        {
-            var np = p.NextDiagonal(d);
-            return Exist(np) ? np : p;
-        }
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        var np = p.NextDiagonal(d);
+        return Exist(np) ? np : p;
     }
 }
